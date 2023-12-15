73ceaf37c567c48f00cd0759b87fec83bdb34099
Stephanie David <stephdaviduk@gmail.com>
2023-12-15 12:40:38 +0000

    manipulated the results from search to access the selected city from the cities array

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,118 +7,87 @@ $(document).ready(function () {
     let city = "Paris";
     //console.log(city);
 
-    // Get today's date
-    let date = new Date();
+    let cities = ["Bexleyheath (Gtr London), UK", "London, UK", "Ottawa, Canada", "Kingston, Jamaica", "Dublin, Ireland", "Athens, Greece"];
 
-    date.setDate(date.getDate());
+    function updatePage(data) {
+        console.log("This is the updatepage function");
 
-    // Calculate dates for one to five days after today
-    let oneDayAfter = new Date(date);
-    oneDayAfter.setDate(date.getDate() + 1);
+    };
 
-    let twoDaysAfter = new Date(date);
-    twoDaysAfter.setDate(date.getDate() + 2);
+    function getLatAndLon() {
+        
+        if (selectedBtn || selectedCity) {
+            //if (selectedBtn) {
+            let city = selectedBtn || selectedCity;
+            //let city = selectedBtn;
+            console.log("I'm here!");
+            let geoURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&appid=" + APIKey; // get lat & lon
+            fetch(geoURL)
+                .then(function (response) {
+                    return response.json();
+                })
+                .then(function (data) {
+                    console.log("Query URL: " + geoURL);
 
-    let threeDaysAfter = new Date(date);
-    threeDaysAfter.setDate(date.getDate() + 3);
+                    // Log the resulting object
+                    console.log("API Response:" + data);
 
-    let fourDaysAfter = new Date(date);
-    fourDaysAfter.setDate(date.getDate() + 4);
-
-    let fiveDaysAfter = new Date(date);
-    fiveDaysAfter.setDate(date.getDate() + 5);
-
-    // Format dates
-    let format = { day: 'numeric', month: 'numeric', year: 'numeric' };
-
-    let currentDate = date.toLocaleDateString('en-UK', format);
-    let oneDayAfterDate = oneDayAfter.toLocaleDateString('en-UK', format);
-    let twoDaysAfterDate = twoDaysAfter.toLocaleDateString('en-UK', format);
-    let threeDaysAfterDate = threeDaysAfter.toLocaleDateString('en-UK', format);
-    let fourDaysAfterDate = fourDaysAfter.toLocaleDateString('en-UK', format);
-    let fiveDaysAfterDate = fiveDaysAfter.toLocaleDateString('en-UK', format);
-
-    let citiesArr = ["Bexleyheath (Gtr London), UK", "London, UK", "Ottawa, Canada", "Kingston, Jamaica", "Dublin, Ireland", "Athens, Greece"];
-
-    $("#search-button").on("click", function (event) {
-        event.preventDefault();
-        // This line grabs the input from the textbox
-
-
-        let city = $("#search-input").val().trim();
-        citiesArr.push(city);
-    });
-
-    const div = document.querySelector('.input-group-append')
-
-    div.addEventListener("click", (event) => {
-        if (event.target.tagName === 'BUTTON') {
-            // console.log(event.target.innerText);
-            selectedBtn = event.target.innerText;
-            console.log(selectedBtn)
+                    // Check if the response is an array and has at least one item
+                    if (Array.isArray(data) && data.length > 0) {
+                        // Access the lat property from the first item in the array
+                        let latitude = data[0].lat;
+                        let longitude = data[0].lon;
+                        console.log("latitude: " + latitude);
+                        console.log("longitude: " + longitude);
+                        return latitude, longitude;
+                    }
+                })
         }
-    })
+    }
 
-    // if button then
-    // elseif search then
-    // else placeholder
-
-    //if (selectedBtn || selectedCity) {
-   // if (selectedBtn) {
-        //let city = selectedBtn || selectedCity;
-  //      let city = selectedBtn;
-        console.log(city);
-  let geoURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&appid=" + APIKey; // get lat & lon
-        fetch(geoURL)
+    function getCurrentAndForcastedWeather(latitude, longitude) {
+        let queryURL = "https://api.openweathermap.org/data/3.0/onecall?lat=" + latitude + "&lon=" + longitude + "&appid=" + APIKey;
+        fetch(queryURL)
             .then(function (response) {
                 return response.json();
             })
-            .then(function (data) {
-                console.log("Query URL: " + geoURL);
-
-                // Log the resulting object
-                console.log("API Response:" + data);
-
-                // Check if the response is an array and has at least one item
-                if (Array.isArray(data) && data.length > 0) {
-                    // Access the lat property from the first item in the array
-                    let latitude = data[0].lat;
-                    let longitude = data[0].lon;
-                    console.log("latitude: " + latitude);
-                    console.log("longitude: " + longitude);
-                    let queryURL = "https://api.openweathermap.org/data/3.0/onecall?lat=" + latitude + "&lon=" + longitude +"&appid=" + APIKey; 
-                    fetch(queryURL)
-                        .then(function (response) {
-                            return response.json();
-                        })
-                        .then(function (data) {
-                            console.log("Query URL: " + queryURL);
-
-                            //if (Array.isArray(data) && data.length > 0) {
-                            if (typeof data === 'object' && data !== null) {
-                                // Access the temp property from the first item in the array
-                                let temperature = data.daily[0].temp.day;
-                                console.log("Temperature: " + temperature);
-                            } else {
-                             console.error("Invalid or empty response from the API.");
-                           }
-                            // Log the resulting object
-                            console.log("API Response:" + data);
-                            return data;
-                        })             
-
-                } else {
-                    console.error("Invalid or empty response from the API.");
-                }
+            .then(function (updatePage) {
+                return data;
             })
-            .catch(function (error) {
-                console.error("Error fetching data:", error);
-            }); 
-
- //}            
-
-
+    }
 /*
+    function populateTheCards() {
+        // Get today's date
+        let date = new Date();
+
+        date.setDate(date.getDate());
+
+        // Calculate dates for one to five days after today
+        let oneDayAfter = new Date(date);
+        oneDayAfter.setDate(date.getDate() + 1);
+
+        let twoDaysAfter = new Date(date);
+        twoDaysAfter.setDate(date.getDate() + 2);
+
+        let threeDaysAfter = new Date(date);
+        threeDaysAfter.setDate(date.getDate() + 3);
+
+        let fourDaysAfter = new Date(date);
+        fourDaysAfter.setDate(date.getDate() + 4);
+
+        let fiveDaysAfter = new Date(date);
+        fiveDaysAfter.setDate(date.getDate() + 5);
+
+        // Format dates
+        let format = { day: 'numeric', month: 'numeric', year: 'numeric' };
+
+        let currentDate = date.toLocaleDateString('en-UK', format);
+        let oneDayAfterDate = oneDayAfter.toLocaleDateString('en-UK', format);
+        let twoDaysAfterDate = twoDaysAfter.toLocaleDateString('en-UK', format);
+        let threeDaysAfterDate = threeDaysAfter.toLocaleDateString('en-UK', format);
+        let fourDaysAfterDate = fourDaysAfter.toLocaleDateString('en-UK', format);
+        let fiveDaysAfterDate = fiveDaysAfter.toLocaleDateString('en-UK', format);
+
         // Calculate the temperature (converted from Kelvin to Celsius)
         let temperatureCelsius = data.current.temp - 273.15;
         let temperatureCelsiusDayAfterOne = data.daily[0].temp.day - 273.15;
@@ -150,16 +119,15 @@ $(document).ready(function () {
         const iconCodeDayAfterFive = data.daily[4].weather[0].icon;
         const iconUrlDayAfterFive = "http://openweathermap.org/img/wn/" + iconCodeDayAfterFive + ".png";
         $("#weather-icon").html("<img src='" + iconUrlDayAfterFive + "' alt='Weather Icon'>");
-
-
-
-
+*/
+/*
         // Current Weather
         document.querySelector('.cityAndCurrentDate').innerHTML = city + ' ' + currentDate + ' ' + '<img src="' + iconUrl + '" alt="Weather Icon">';
         document.querySelector('.currentTemp').textContent = "Temp: " + temperatureCelsius.toFixed(2) + "°C";
         document.querySelector('.currentWind').textContent = "Wind: " + data.current.wind_speed + " m/s";
         document.querySelector('.currentHumidity').textContent = "Humidity: " + data.current.humidity + "%";
-
+*/
+/*    
         // Forecast over 5 days
         document.querySelector('.forecastDateOne').innerHTML = oneDayAfterDate;
         document.querySelector('.forecastIconOne').innerHTML = '<img src="' + iconUrlDayAfterOne + '" alt="Weather Icon">';
@@ -190,16 +158,28 @@ $(document).ready(function () {
         document.querySelector('.forecastTempFive').innerHTML = "Temp: " + temperatureCelsiusDayAfterFive.toFixed(2) + "°C";
         document.querySelector('.forecastWindFive').textContent = "Wind: " + data.daily[4].wind_speed + " m/s";
         document.querySelector('.forecastHumidityFive').textContent = "Humidity: " + data.daily[4].humidity + "%";
-
-
-
-
-    })
-    .catch(function (error) {
-        console.error("Error fetching data:", error);
-
-    });
+    };
 */
 
+
+$("#search-button").on("click", function (event) {
+    event.preventDefault();
+    // This line grabs the input from the textbox
+    let city = $("#search-input").val().trim();
+    cities.push(city);
+    //const last = array[array.length - 1]
+    selectedCity = cities[cities.length - 1];
+    console.log("selected city: " + selectedCity); 
+});
+
+const div = document.querySelector('.input-group-append')
+
+div.addEventListener("click", (event) => {
+    if (event.target.tagName === 'BUTTON') {
+        // console.log(event.target.innerText);
+        selectedBtn = event.target.innerText;
+        console.log(selectedBtn)
+    }
+})
 
 });
